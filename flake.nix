c0293ace989f6558e103d38a483b20b2f02879d2
nick <nick@thecjs.co>
2026-04-08 14:35:52 +0100

    fix: correct Nix flake syntax (use attribute sets not JSON)

--- flake.nix
+++ flake.nix
@@ -1,29 +1,20 @@
 {
-  "inputs": {
-    "nixpkgs": {
-      "follows": "nixpkgs",
-      "url": "github:NixOS/nixpkgs/nixos-unstable"
-    },
-    "disko": {
-      "url": "github:nix-community/disko"
-    }
-  },
-  "outputs": {
-    "nixosConfigurations.generic-vm": {
-      "modules": [
-        ./modules/disko.nix,
-        ./modules/ssh.nix,
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    disko.url = "github:nix-community/disko";
+  };
+
+  outputs = { self, nixpkgs, disko }: {
+    nixosConfigurations.generic-vm = nixpkgs.lib.nixosSystem {
+      modules = [
+        disko.nixosModules.disko
+        ./modules/ssh.nix
         {
-          "imports": [
-            "${inputs.nixpkgs}/nixos"
-          ],
-          "config": {
-            "bootloader.systemd-boot.enable = true;",
-            "bootloader.efi.canTouchEfiVariables = true;",
-            "networking.useDHCP = true;"
-          }
+          boot.loader.systemd-boot.enable = true;
+          boot.loader.efi.canTouchEfiVariables = true;
+          networking.useDHCP = true;
         }
-      ]
-    }
-  }
+      ];
+    };
+  };
 }
