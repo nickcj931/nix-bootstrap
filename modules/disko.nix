8741b36d7983084e05616b8b55dcdd4e90544cb7
nick <nick@thecjs.co>
2026-04-08 14:52:41 +0100

    fix: align disko layout with working disk.main structure

--- modules/disko.nix
+++ modules/disko.nix
@@ -1,60 +1,63 @@
-{ vmConfig, ... }:
+{ lib, vmConfig, ... }:
 let
+  diskDevice = lib.mkDefault vmConfig.diskDevice;
   commonMountOptions = [ "compress=zstd" "noatime" ];
   relatimeMountOptions = [ "compress=zstd" ];
 in
 {
-  disko.devices.disk.main = {
-    type = "disk";
-    device = vmConfig.diskDevice;
-    content = {
-      type = "gpt";
-      partitions = {
-        ESP = {
-          label = "EFI";
-          size = "512M";
-          type = "EF00";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-            mountOptions = [ "umask=0077" ];
+  disko.devices = {
+    disk.main = {
+      type = "disk";
+      device = diskDevice;
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            label = "EFI";
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
           };
-        };
 
-        root = {
-          size = vmConfig.diskSize;
-          content = {
-            type = "btrfs";
-            extraArgs = [ "-f" ];
-            subvolumes = {
-              "/@root" = {
-                mountpoint = "/";
-                mountOptions = relatimeMountOptions;
-              };
-              "/@home" = {
-                mountpoint = "/home";
-                mountOptions = relatimeMountOptions;
-              };
-              "/@nix" = {
-                mountpoint = "/nix";
-                mountOptions = commonMountOptions;
-              };
-              "/@var" = {
-                mountpoint = "/var";
-                mountOptions = relatimeMountOptions;
-              };
-              "/@log" = {
-                mountpoint = "/var/log";
-                mountOptions = commonMountOptions;
-              };
-              "/@snapshots" = {
-                mountpoint = "/.snapshots";
-                mountOptions = commonMountOptions;
-              };
-              "/@swap" = {
-                mountpoint = "/.swapvol";
-                swap.swapfile.size = vmConfig.swapSize;
+          root = {
+            size = vmConfig.diskSize;
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "/@root" = {
+                  mountpoint = "/";
+                  mountOptions = relatimeMountOptions;
+                };
+                "/@home" = {
+                  mountpoint = "/home";
+                  mountOptions = relatimeMountOptions;
+                };
+                "/@nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = commonMountOptions;
+                };
+                "/@var" = {
+                  mountpoint = "/var";
+                  mountOptions = relatimeMountOptions;
+                };
+                "/@log" = {
+                  mountpoint = "/var/log";
+                  mountOptions = commonMountOptions;
+                };
+                "/@snapshots" = {
+                  mountpoint = "/.snapshots";
+                  mountOptions = commonMountOptions;
+                };
+                "/@swap" = {
+                  mountpoint = "/.swapvol";
+                  swap.swapfile.size = vmConfig.swapSize;
+                };
               };
             };
           };
